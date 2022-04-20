8ddd8072b9ce98e643cc346899998194d43bafdb
shinyelee <shinye0213@naver.com>
2022-04-20 15:34:36 +0900

    rollback

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -9,7 +9,7 @@ import android.widget.Button
 import android.widget.Toast
 
 // 메인화면
-class MainActivity : AppCompatActivity(), MyCustomDialogInterface {
+class MainActivity : AppCompatActivity() {
 
     val TAG: String = "로그"
 
@@ -20,26 +20,6 @@ class MainActivity : AppCompatActivity(), MyCustomDialogInterface {
         // 그릴 xml 뷰 파일을 연결(설정) 시켜줌
         setContentView(R.layout.activity_main)
 
-    }
-
-    fun onDialogBtnClicked(view: View) {
-        Log.d(TAG, "MainActivity - onDialogBtnClicked() called")
-
-        val myCustomDialog = MyCustomDialog(this, this)
-
-        myCustomDialog.show()
-    }
-
-    // 구독 버튼 클릭
-    override fun onSubscribeBtnClicked() {
-        Log.d(TAG, "MainActivity - onSubscribeBtnClicked() called")
-        Toast.makeText(this, "구독!", Toast.LENGTH_LONG).show()
-    }
-
-    // 좋아요 버튼 클릭
-    override fun onLikeBtnClicked() {
-        Log.d(TAG, "MainActivity - onLikeBtnClicked() called")
-        Toast.makeText(this, "좋아요!", Toast.LENGTH_LONG).show()
     }
 
 }
