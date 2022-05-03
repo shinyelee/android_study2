f4996835b8fcef8257894dd462dacdeaf608d11e
shinyelee <shinye0213@naver.com>
2022-05-03 09:23:30 +0900

    listView (2)

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Toast
@@ -25,8 +26,14 @@ class MainActivity : AppCompatActivity() {
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val item = arrayOf("0번", "1번", "2번", "3번", "4번", "5번", "6번", "7번", "8번", "9번", "10번", "11번", "12번", "13번", "14번", "15번")
+        val item = arrayOf("1번", "2번", "3번", "4번", "5번", "6번", "7번", "8번", "9번", "10번", "11번", "12번", "13번", "14번", "15번")
         binding.listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, item)
+
+        // 각 아이템 클릭하면 해당하는 토스트 메시지 띄움
+        binding.listView.onItemClickListener = AdapterView.OnItemClickListener {parent, view, position, id ->
+            val selectItem = parent.getItemAtPosition(position)
+            Toast.makeText(this, selectItem.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroy() {
