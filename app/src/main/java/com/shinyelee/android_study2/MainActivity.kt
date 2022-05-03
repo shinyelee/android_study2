3a27dee9e2a32623c9009b492c82ba10f887e4b0
shinyelee <shinye0213@naver.com>
2022-05-03 20:29:13 +0900

    NavigationView (2) ViewPager

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -1,19 +1,54 @@
 package com.shinyelee.android_study2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.*
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var vBinding : ActivityMainBinding? = null
+    private val binding get() = vBinding!!
+
+    var viewList = ArrayList<View>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        vBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewList.add(layoutInflater.inflate(R.layout.fragment_home, null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_favorite, null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_profile, null))
+
+        binding.viewPager.adapter = pagerAdapter()
+
     }
 
+    inner class pagerAdapter : PagerAdapter() {
+
+        override fun getCount() = viewList.size
+
+        override fun isViewFromObject(view: View, `object`: Any) = view == `object`
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            var curView = viewList[position]
+            binding.viewPager.addView(curView)
+            return curView
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            binding.viewPager.removeView(`object` as View)
+        }
+
+    }
+
+    override fun onDestroy() {
+        vBinding = null
+        super.onDestroy()
+    }
 }
