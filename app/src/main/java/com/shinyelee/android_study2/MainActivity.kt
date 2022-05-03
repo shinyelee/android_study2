895134ab17eb160bdc80a8f39f75f20e422f1396
shinyelee <shinye0213@naver.com>
2022-05-03 12:54:27 +0900

    rollback

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -11,21 +11,9 @@ import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var vBinding : ActivityMainBinding? = null
-    private val binding get() = vBinding!!
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        vBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = CustomAdapter(this)
-    }
-
-    override fun onDestroy() {
-        vBinding = null
-        super.onDestroy()
+        setContentView(R.layout.activity_main)
     }
 
 }
