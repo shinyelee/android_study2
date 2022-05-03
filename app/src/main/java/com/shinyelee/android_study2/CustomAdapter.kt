8cc966d90514f4031b5ce1b4228185738a1f9ca0
shinyelee <shinye0213@naver.com>
2022-05-03 11:58:27 +0900

    recyclerView (1)

--- app/src/main/java/com/shinyelee/android_study2/CustomAdapter.kt
+++ app/src/main/java/com/shinyelee/android_study2/CustomAdapter.kt
@@ -1,38 +1,24 @@
 package com.shinyelee.android_study2
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-// 프로필사진(이미지뷰), 이름(텍스트뷰)
-// 이미지를 숫자로 참조하기 위해 int로 받음
-class Data(val profile: Int, val name: String)
+// 매개변수 v : View
+// 상속 RecyclerView.ViewHolder(v)
+class CustomViewHolder(v : View) : RecyclerView.ViewHolder(v) {}
 
-// 매개변수, 상속
-class CustomAdapter(val context: Context, val DataList: ArrayList<Data>) : BaseAdapter()
-{
+class CustomAdapter() : RecyclerView.Adapter<CustomViewHolder>() {
 
-    override fun getCount() = DataList.size
-
-    // any
-    override fun getItem(position: Int) = DataList[position]
-
-    // long
-    override fun getItemId(position: Int) = 0L
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.custom_list, null)
-        val profile = view.findViewById<ImageView>(R.id.iv_custom)
-        val name = view.findViewById<TextView>(R.id.tv_custom)
-        val data = DataList[position]
-
-        profile.setImageResource(data.profile)
-        name.text = data.name
-        return view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val cellForRow = LayoutInflater.from(parent.context).inflate(R.layout.custom_list, parent, false)
+        return CustomViewHolder(cellForRow)
     }
 
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = 10
 }
--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,24 +16,37 @@ class MainActivity : AppCompatActivity() {
     private var vBinding : ActivityMainBinding? = null
     private val binding get() = vBinding!!
 
-    var DataList = arrayListOf(
-        Data(R.drawable.paw, "0번"),
-        Data(R.drawable.paw, "1번"),
-        Data(R.drawable.paw, "2번"),
-        Data(R.drawable.paw, "3번")
-    )
+//    val DataList = arrayListOf(
+//        Data(R.drawable.paw, "1번"),
+//        Data(R.drawable.paw, "2번"),
+//        Data(R.drawable.paw, "3번"),
+//        Data(R.drawable.paw, "4번"),
+//        Data(R.drawable.paw, "5번"),
+//        Data(R.drawable.paw, "6번"),
+//        Data(R.drawable.paw, "7번"),
+//        Data(R.drawable.paw, "8번"),
+//        Data(R.drawable.paw, "9번"),
+//        Data(R.drawable.paw, "10번"),
+//        Data(R.drawable.paw, "11번"),
+//        Data(R.drawable.paw, "12번"),
+//        Data(R.drawable.paw, "13번"),
+//        Data(R.drawable.paw, "14번"),
+//        Data(R.drawable.paw, "15번"),
+//        Data(R.drawable.paw, "16번"),
+//        Data(R.drawable.paw, "17번"),
+//        Data(R.drawable.paw, "18번"),
+//        Data(R.drawable.paw, "19번"),
+//        Data(R.drawable.paw, "20번")
+//    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
 
-        binding.listView.adapter = CustomAdapter(this, DataList)
-
-//        binding.listView.onItemClickListener = AdapterView.OnItemClickListener {parent, view, position, id ->
-//            val selectItem = parent.getItemAtPosition(position) as Data
-//            Toast.makeText(this, selectItem.name, Toast.LENGTH_SHORT).show()
-//        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = CustomAdapter()
     }
 
     override fun onDestroy() {
