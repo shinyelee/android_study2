573edc27841ae44fb0e9f4dabe6faece2382766d
shinyelee <shinye0213@naver.com>
2022-05-03 09:37:48 +0900

    listView (3)

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -11,7 +11,6 @@ import android.widget.Button
 import android.widget.Toast
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
-// 메인화면
 class MainActivity : AppCompatActivity() {
 
     val TAG: String = "로그"
@@ -19,21 +18,24 @@ class MainActivity : AppCompatActivity() {
     private var vBinding : ActivityMainBinding? = null
     private val binding get() = vBinding!!
 
-    // 뷰 생성
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var DataList = arrayListOf(
+        Data(R.drawable.paw, "0번"),
+        Data(R.drawable.paw, "1번"),
+        Data(R.drawable.paw, "2번"),
+        Data(R.drawable.paw, "3번")
+    )
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val item = arrayOf("1번", "2번", "3번", "4번", "5번", "6번", "7번", "8번", "9번", "10번", "11번", "12번", "13번", "14번", "15번")
-        binding.listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, item)
+        binding.listView.adapter = CustomAdapter(this, DataList)
 
-        // 각 아이템 클릭하면 해당하는 토스트 메시지 띄움
-        binding.listView.onItemClickListener = AdapterView.OnItemClickListener {parent, view, position, id ->
-            val selectItem = parent.getItemAtPosition(position)
-            Toast.makeText(this, selectItem.toString(), Toast.LENGTH_SHORT).show()
-        }
+//        binding.listView.onItemClickListener = AdapterView.OnItemClickListener {parent, view, position, id ->
+//            val selectItem = parent.getItemAtPosition(position) as Data
+//            Toast.makeText(this, selectItem.name, Toast.LENGTH_SHORT).show()
+//        }
     }
 
     override fun onDestroy() {
