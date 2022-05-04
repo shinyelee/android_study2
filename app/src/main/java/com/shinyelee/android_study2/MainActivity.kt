347804682429ce800e49b9a73241c701b9027e90
shinyelee <shinye0213@naver.com>
2022-05-04 16:00:55 +0900

    ToggleButton

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -2,10 +2,6 @@ package com.shinyelee.android_study2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,16 +15,10 @@ class MainActivity : AppCompatActivity() {
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 체크 기본값
-        binding.radioGroup.check(R.id.radioButton3)
-        // 체크 해제
-//        binding.radioGroup.clearCheck()
-
-        when(binding.radioGroup.checkedRadioButtonId) {
-            R.id.radioButton1 -> println("1번")
-            R.id.radioButton2 -> println("2번")
-            R.id.radioButton3 -> println("3번")
+        binding.toggleButton.setOnCheckedChangeListener { compoundButton, b ->
+            println(b)
         }
+        binding.toggleButton.isChecked = true
 
     }
 
