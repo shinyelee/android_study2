dbd634f906729de3a034bfb3ad4bb48ba7f21de2
shinyelee <shinye0213@naver.com>
2022-05-04 15:45:02 +0900

    RadioGroup, RadioButton

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -18,15 +18,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-        binding.checkBox.setOnCheckedChangeListener { compoundButton, b ->
-            println(b)
+
+        // 체크 기본값
+        binding.radioGroup.check(R.id.radioButton3)
+        // 체크 해제
+//        binding.radioGroup.clearCheck()
+
+        when(binding.radioGroup.checkedRadioButtonId) {
+            R.id.radioButton1 -> println("1번")
+            R.id.radioButton2 -> println("2번")
+            R.id.radioButton3 -> println("3번")
         }
-        binding.checkBox.isChecked = true
+
     }
 
     override fun onDestroy() {
         vBinding = null
         super.onDestroy()
     }
+
 }
