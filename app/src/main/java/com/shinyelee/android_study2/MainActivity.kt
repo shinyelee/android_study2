51c6864e779b189ee8607dd3d8427616f71ba448
shinyelee <shinye0213@naver.com>
2022-05-05 23:08:47 +0900

    WebView

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.shinyelee.android_study2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import android.widget.Toast
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,11 +11,33 @@ class MainActivity : AppCompatActivity() {
     private var vBinding : ActivityMainBinding? = null
     private val binding get() = vBinding!!
 
+    private var lastTimeBackPressed = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.loadUrl("https://shinye0213.tistory.com/")
+
+    }
+
+    override fun onBackPressed() {
+
+        if(binding.webView.canGoBack()) binding.webView.goBack()
+
+        else {
+
+            if(System.currentTimeMillis() - lastTimeBackPressed < 1500) {
+                finish()
+                return
+            }
+            lastTimeBackPressed = System.currentTimeMillis()
+            Toast.makeText(this, "'뒤로' 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+
+        }
 
     }
 
