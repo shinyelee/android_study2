36a0db9b6c0cab7eebf01722513929bdd2ab43bf
shinyelee <shinye0213@naver.com>
2022-05-06 23:20:48 +0900

    VideoView (1)

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shinyelee.android_study2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebViewClient
+import android.widget.MediaController
 import android.widget.Toast
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
@@ -11,33 +12,19 @@ class MainActivity : AppCompatActivity() {
     private var vBinding : ActivityMainBinding? = null
     private val binding get() = vBinding!!
 
-    private var lastTimeBackPressed = 0L
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.webView.webViewClient = WebViewClient()
-        binding.webView.loadUrl("https://shinye0213.tistory.com/")
+        val videoPath = "android.resource://" + getPackageName() + "/" + R.raw.joinfail
+        binding.videoView.setVideoPath(videoPath)
 
-    }
-
-    override fun onBackPressed() {
-
-        if(binding.webView.canGoBack()) binding.webView.goBack()
-
-        else {
-
-            if(System.currentTimeMillis() - lastTimeBackPressed < 1500) {
-                finish()
-                return
-            }
-            lastTimeBackPressed = System.currentTimeMillis()
-            Toast.makeText(this, "'뒤로' 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
-
-        }
+        val mediaController = MediaController(this)
+        binding.videoView.setMediaController(mediaController)
+        mediaController.setAnchorView(binding.videoView)
+        binding.videoView.keepScreenOn = true
 
     }
 
