671863b11443c8fdbd99957bbe39d439627d0768
shinyelee <shinye0213@naver.com>
2022-05-08 23:54:07 +0900

    CalendarView (2)

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shinyelee.android_study2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calendarView.setOnDateChangeListener { calendarView, year, month, day ->
+        // 월(month)은 1이 아니라 0부터 시작하니 주의!
+//            println(year.toString() + "/" + month.toString() + "/" + day.toString())
+        }
+        val calendar = Calendar.getInstance()
+        // 기본값 세팅하기
+        calendar.set(2022, 5, 15)
+        binding.calendarView.setDate(calendar.timeInMillis)
 
     }
 
