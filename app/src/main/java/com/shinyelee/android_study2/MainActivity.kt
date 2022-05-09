9d1a5825921075f49d7b50c379eb0be82a7f3a72
shinyelee <shinye0213@naver.com>
2022-05-09 16:55:38 +0900

    Progress Bar (1) Circular

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -16,15 +16,6 @@ class MainActivity : AppCompatActivity() {
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calendarView.setOnDateChangeListener { calendarView, year, month, day ->
-        // 월(month)은 1이 아니라 0부터 시작하니 주의!
-//            println(year.toString() + "/" + month.toString() + "/" + day.toString())
-        }
-        val calendar = Calendar.getInstance()
-        // 기본값 세팅하기
-        calendar.set(2022, 5, 15)
-        binding.calendarView.setDate(calendar.timeInMillis)
-
     }
 
     override fun onDestroy() {
