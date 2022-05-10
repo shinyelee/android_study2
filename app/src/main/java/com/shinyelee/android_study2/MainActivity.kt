ea6ac2ec48016e9a745bf147158ee0d6bfae9483
shinyelee <shinye0213@naver.com>
2022-05-10 23:24:15 +0900

    Spinner

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -2,6 +2,9 @@ package com.shinyelee.android_study2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,11 +12,28 @@ class MainActivity : AppCompatActivity() {
     private var vBinding : ActivityMainBinding? = null
     private val binding get() = vBinding!!
 
+    var items = arrayOf("나연", "정연", "모모", "사나", "지효", "미나", "다현", "채영", "쯔위")
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val myAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        binding.spinner.adapter = myAdapter
+        binding.spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                println(p2)
+                items[p2]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                println("선택 안 함")
+            }
+
+        }
 
     }
 
