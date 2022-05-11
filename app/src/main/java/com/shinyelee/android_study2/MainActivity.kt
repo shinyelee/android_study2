8fdc85b1bd85b11e9e7dd7148000f30f6ceb8e16
shinyelee <shinye0213@naver.com>
2022-05-11 14:22:44 +0900

    Guideline

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -13,31 +13,11 @@ class MainActivity : AppCompatActivity() {
     private var vBinding : ActivityMainBinding? = null
     private val binding get() = vBinding!!
 
-    private var MyList = arrayOf("yeji", "ryujin", "chaeryeong", "yuna", "lia")
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener {
-
-            override fun onQueryTextSubmit(p0: String?): Boolean {
-                println(p0 + " 입력완료")
-                return true
-            }
-
-            override fun onQueryTextChange(p0: String?): Boolean {
-                if(p0 != "") {
-                    val curList = MyList.filter { x -> x.toLowerCase().contains(p0?.toLowerCase().toString()) }
-                    for (i in curList) println(i)
-                    println("----------")
-                }
-                return true
-            }
-
-        })
 
     }
 
