74c3c785fd25bffd0abaac514c2393a4730e7de8
shinyelee <shinye0213@naver.com>
2022-05-12 18:32:23 +0900

    Toolbar

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.shinyelee.android_study2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -19,6 +21,21 @@ class MainActivity : AppCompatActivity() {
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.first -> println("첫번째 선택")
+            R.id.second -> println("두번째 선택")
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroy() {
