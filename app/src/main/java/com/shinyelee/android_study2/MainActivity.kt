978517b795124bdcef650a17a5c8b02c7a427caf
shinyelee <shinye0213@naver.com>
2022-05-17 22:06:17 +0900

    Music Streaming

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shinyelee.android_study2
 
 import android.content.Context
 import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -18,14 +19,17 @@ class MainActivity : AppCompatActivity() {
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btn.setOnClickListener { Click(binding.btn) }
+
     }
 
-    fun Audio(v: View) {
-        var audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-        audioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL)
-        audioManager.getStreamVolume(AudioManager.STREAM_RING)
-        audioManager.getStreamVolume(AudioManager.STREAM_ALARM)
+    // 저작권 문제로 음악 파일은 올리지 않았음
+    fun Click(v: View) {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.iu_friday)
+        mediaPlayer.start()
+        // 중지할 땐 반대로
+//        mediaPlayer.stop()
+        mediaPlayer.setOnCompletionListener { mediaPlayer.release() }
     }
 
     override fun onDestroy() {
