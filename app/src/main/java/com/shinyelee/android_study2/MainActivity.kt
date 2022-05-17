deb7b41e5fcbcc3e2e4330a1162f92b319ba57fb
shinyelee <shinye0213@naver.com>
2022-05-17 18:01:23 +0900

    Dialog

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.shinyelee.android_study2
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.os.Build
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.NotificationCompat
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,28 +18,32 @@ class MainActivity : AppCompatActivity() {
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn.setOnClickListener { ShowNotification("제목", "내용") }
-    }
+        binding.btn.setOnClickListener {
 
-    fun ShowNotification(Title: String, Body: String) {
+            var ad = AlertDialog.Builder(this)
 
-        val pending = PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java), PendingIntent.FLAG_CANCEL_CURRENT)
+            ad.setIcon(R.mipmap.ic_launcher)
+            ad.setTitle("제목")
+            ad.setMessage("내용")
+            ad.setView(EditText(this))
 
-        val builder = NotificationCompat.Builder(this, "id")
+            ad.setPositiveButton("Yes", object:DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    println("Yes 클릭")
+                    p0?.dismiss()
+                }
+            })
 
-        builder.setSmallIcon(R.drawable.ic_baseline_create_24)
-            .setContentTitle(Title)
-            .setContentText(Body)
-            .setContentIntent(pending)
-            .setAutoCancel(true)
-            .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+            ad.setNegativeButton("No", object:DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    println("No 클릭")
+                    p0?.dismiss()
+                }
+            })
 
-        val NManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            ad.show()
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NManager.createNotificationChannel(NotificationChannel("id", "name", NotificationManager.IMPORTANCE_HIGH))
         }
-        NManager.notify(0, builder.build())
 
     }
 
