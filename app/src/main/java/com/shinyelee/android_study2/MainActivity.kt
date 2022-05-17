a2818598dca230410c727d81b6a9c156dc523f1d
shinyelee <shinye0213@naver.com>
2022-05-17 17:24:10 +0900

    Push Notification

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.shinyelee.android_study2
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.SearchView
-import com.google.android.material.tabs.TabLayout
+import androidx.core.app.NotificationCompat
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -25,13 +23,29 @@ class MainActivity : AppCompatActivity() {
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn.setOnClickListener { Vibration(1000L) }
-
+        binding.btn.setOnClickListener { ShowNotification("제목", "내용") }
     }
 
-    fun Vibration(len:Long) {
-        val vibrator = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as Vibrator
-        vibrator.vibrate(len)
+    fun ShowNotification(Title: String, Body: String) {
+
+        val pending = PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java), PendingIntent.FLAG_CANCEL_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, "id")
+
+        builder.setSmallIcon(R.drawable.ic_baseline_create_24)
+            .setContentTitle(Title)
+            .setContentText(Body)
+            .setContentIntent(pending)
+            .setAutoCancel(true)
+            .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+
+        val NManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NManager.createNotificationChannel(NotificationChannel("id", "name", NotificationManager.IMPORTANCE_HIGH))
+        }
+        NManager.notify(0, builder.build())
+
     }
 
     override fun onDestroy() {
