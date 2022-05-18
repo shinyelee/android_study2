50ee7f7c61ef948f9c0debfbd03c8476ba8d2453
shinyelee <shinye0213@naver.com>
2022-05-18 15:47:33 +0900

    Wi-Fi

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -1,49 +1,29 @@
 package com.shinyelee.android_study2
 
 import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity() {
 
     private var vBinding : ActivityMainBinding? = null
     private val binding get() = vBinding!!
-
-    lateinit var sensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btn.setOnClickListener { NetworkCheck(binding.btn) }
     }
 
-    override fun onResume() {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL)
-        super.onResume()
+    fun NetworkCheck(v: View) {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        println(connectivityManager.activeNetworkInfo?.isConnected)
     }
-
-    override fun onPause() {
-        sensorManager.unregisterListener(this)
-        super.onPause()
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        val x = event?.values?.get(0) as Float
-        val y = event?.values?.get(1) as Float
-        val z = event?.values?.get(2) as Float
-        println(x.toString() + ", " + y.toString() + ", " + z.toString())
-    }
-
     override fun onDestroy() {
         vBinding = null
         super.onDestroy()
