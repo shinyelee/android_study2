943553e0470eb6e35329262f538a3c454f7f3140
shinyelee <shinye0213@naver.com>
2022-05-18 15:07:26 +0900

    Motion Sensor

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.shinyelee.android_study2
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var vBinding : ActivityMainBinding? = null
     private val binding get() = vBinding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var sensorManager: SensorManager
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
+    override fun onResume() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL)
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager.unregisterListener(this)
+        super.onPause()
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val x = event?.values?.get(0) as Float
+        val y = event?.values?.get(1) as Float
+        val z = event?.values?.get(2) as Float
+        println(x.toString() + ", " + y.toString() + ", " + z.toString())
     }
 
     override fun onDestroy() {
