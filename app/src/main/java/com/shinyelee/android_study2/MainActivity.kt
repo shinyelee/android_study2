e19a63d6464a77232b1e7660f5b0da388722fbf3
shinyelee <shinye0213@naver.com>
2022-05-18 23:30:50 +0900

    Include

--- app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
+++ app/src/main/java/com/shinyelee/android_study2/MainActivity.kt
@@ -2,10 +2,6 @@ package com.shinyelee.android_study2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.shinyelee.android_study2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,14 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         vBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.btn1.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.frameLayout, OneFragment()).commit()
-        }
-
-        binding.btn2.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.frameLayout, TwoFragment()).commit()
-        }
     }
 
     override fun onDestroy() {
@@ -32,20 +20,4 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
-}
-
-class OneFragment: Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.frag1, container, false)
-}
-
-class TwoFragment: Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.frag2, container, false)
 }
